d8d45b65b9695c350eb317ccba3efeff5eeccc09
xNovyy <jakunow887@student.polsl.pl>
2023-01-02 23:43:13 +0100

    Added CMake TODO: DataGeneratorWithCuda.cu - add includes for windows
    Test

---
+++ src/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Aggregator.h>
+#include <DataGeneratorWithCuda.h>
+#include <DataGenerator.h>
+#include <Logger.h>
+#include <Utilities.h>
+#include <Validator.h>
+#include <CudaAggergatorSimple.h>
+
+
+
+int main()
+{
+	std::cout<<"Hello";
+	return 0;
+}
